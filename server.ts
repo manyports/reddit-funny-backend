41bbb87670cbfbe2e7c6b31343a8132393a6e096
ера <157960666+manyports@users.noreply.github.com>
2024-06-15 16:07:11 +0500

    Initial

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { handleRequest } from '.,/../../backend/api/startCron';
+import { handleRequest } from './api/startCron';
 
 const app = express();
 const port = process.env.PORT || 8080; 
